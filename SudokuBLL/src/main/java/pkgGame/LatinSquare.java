aacb60cd4a7f1c3c35fe86cf86b45c444af31e24
NoahStiebritz <70611826+NoahStiebritz@users.noreply.github.com>
2020-09-17 12:10:57 -0400

    Add files via upload

---
+++ SudokuBLL/src/main/java/pkgGame/LatinSquare.java
@@ -0,0 +1,18 @@
+package pkgGame;
+
+public class LatinSquare {
+	
+	private int[][] LatinSquare;
+
+	public LatinSquare() {
+		super();
+	}
+
+	public LatinSquare(int[][] latinSquare) {
+		super();
+		LatinSquare = latinSquare;
+	}
+	
+	
+
+}
